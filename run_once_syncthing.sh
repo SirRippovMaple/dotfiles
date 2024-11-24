24e42625eef82d1261c6bc83ea801310324a378d
Sir Rippov the Maple <git@trumpi.co.za>
2024-11-23 23:18:16 -0500

    Remove syncthing

--- run_once_syncthing.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env /bin/bash
-
-if [ ! -f ~/.config/syncthing/config.xml ]; then
-    bw get notes syncthing-config.sh > /tmp/syncthing-config.sh 
-    chmod +x /tmp/syncthing-config.sh
-    /tmp/syncthing-config.sh
-    rm /tmp/syncthing-config.sh
-fi

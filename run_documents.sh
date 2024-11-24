796f882c02097c2b2acb1490a4e24dca5e5c7475
Sir Rippov the Maple <git@trumpi.co.za>
2024-11-23 23:18:16 -0500

    Cron

--- run_documents.sh
+++ run_documents.sh
@@ -5,7 +5,13 @@ retVal=$?
 if [ $retVal -ne 0 ]; then
     [ -f ~/Documents ] && mv ~/Documents ~/Documents.bak
     git clone git@trumpi-nas:documents ~/Documents
-    pushd ~/Documents
+    cd ~/Documents
     git config --local commit.gpgsign false
     git config --local branch.master.sync true
+fi
+
+if [[ $(crontab -l | grep -Ev "^(#|$)" | grep -q 'sync-documents'; echo $?) == 1 ]]; then
+    set -f
+    echo "$(crontab -l ; echo '*/15 * * * * '"$HOME"'/.config/cron/sync-documents')" | crontab -
+    set +f
 fi

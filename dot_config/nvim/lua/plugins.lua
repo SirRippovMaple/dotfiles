12fd6a2f7d07c10a9514ecd089f69d11bd5eb1f6
Sir Rippov the Maple <git@trumpi.co.za>
2024-11-23 23:17:45 -0500

    nvim-tree auto close.

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -24,7 +24,7 @@ return require('packer').startup(
                 config.setup {
                     options = {
                         icons_enabled = true,
-                        theme = 'onedart'
+                        theme = 'onedark'
                     }
                 }
             end
@@ -79,6 +79,16 @@ return require('packer').startup(
                         },
                     }
                 }
+            end,
+            run = function()
+                vim.api.nvim_create_autocmd({ "BufEnter" }, {
+                    nested = true,
+                    callback = function()
+                        if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil then
+                            vim.cmd "quit"
+                        end
+                    end
+                })
             end
         }
 

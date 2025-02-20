9ebee89a6067f09c00792cb0e9d00c34aa4c1cf3
Sir Rippov the Maple <git@trumpi.co.za>
2025-02-20 16:44:53 -0500

    Git prepare

---
+++ dot_config/git/executable_prepare.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+git fetch upstream
+if [ $# -eq 1 ]; then
+  main_branch="$(git branch -r | grep "  upstream/$1" | xargs)" || \
+  main_branch="upstream/$(git remote show upstream | sed -n '/HEAD branch/s/.*: //p')"
+elif [ $# -eq 2 ]; then
+  main_branch="upstream/$2"
+else
+  exit 1
+fi
+
+for branch in $(git branch -vv | grep ': gone]' | awk '{print $1}'); do
+  git branch -D "$branch"
+done
+git checkout -b "$1" "$main_branch" && git push -u origin "$1"
+git lprune

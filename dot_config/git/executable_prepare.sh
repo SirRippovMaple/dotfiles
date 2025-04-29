7f9dd324ad49db3cd638051e383364a964cdc2a4
Sir Rippov the Maple <git@trumpi.co.za>
2025-04-28 20:21:25 -0400

    Fix prepare script.

--- dot_config/git/executable_prepare.sh
+++ dot_config/git/executable_prepare.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 git fetch upstream
 if [ $# -eq 1 ]; then
-  main_branch="$(git branch -r | grep "  upstream/$1" | xargs)" || \
-  main_branch="upstream/$(git remote show upstream | sed -n '/HEAD branch/s/.*: //p')"
+  main_branch="$(git branch -r | grep "  upstream/$1" | xargs)"
+  if [ -z "${main_branch}" ]; then
+    main_branch="upstream/$(git remote show upstream | sed -n '/HEAD branch/s/.*: //p')"
+  fi
 elif [ $# -eq 2 ]; then
   main_branch="upstream/$2"
 else
